92c3a847261b173345d6562eb2e3f7aa2f6318df
Kevin Britt <kbritt510@gmail.com>
2020-07-29 18:58:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,6 @@
 def oxford_comma(array)
   array.each do |element|
-    if array.size == 1
-      element.join("")
-    elsif array.size == 2
-      element.join("and")
-    elsif array.size == 3
-      element.join(",", "and")
-    else 
-      element.join(",", "and")
+  element[0].join("")
+  element[1].join("and")
   end
-end
 end
