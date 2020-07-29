99163f4013d066cbfb517bd76539bb28e67ec95d
Kevin Britt <kbritt510@gmail.com>
2020-07-29 19:12:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,12 @@
 def oxford_comma(array)
   new_string = ""
   array.each do |element|
+    
     if array.length == 1
     new_string = array.join("")
+    elsif array.length == 2
+    new_string = array.join("and")
+    
     end
   end
   return new_string
