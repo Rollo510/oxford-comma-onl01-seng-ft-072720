e2b3c4e16030a6f53f1d6624fc8b4261d2bfc667
Kevin Britt <kbritt510@gmail.com>
2020-07-29 19:47:49 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  new_string = ""
+  array.each do |element|
+    
+    if array.length == 1
+    new_string = array.join("")
+    elsif array.length == 2
+    new_string = array.join(" and ")
+    elsif array.length == 3
+    new_string = array[0..1].join(', ') + ", and " + array[-1]
+    else array.length > 3
+    new_string = array[0..-2].join(', ') + ", and " + array[-1]
+    end
+  end
+  return new_string
 end
