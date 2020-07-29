13a65155d486434a7ac74978e0b543c67dbd1b8e
Kevin Britt <kbritt510@gmail.com>
2020-07-29 19:42:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ def oxford_comma(array)
     elsif array.length == 2
     new_string = array.join(" and ")
     elsif array.length >= 3
-    new_string = array[0..1].join(', ') + " and " + array[-1]
+    new_string = array[0..3].join(', ') + " and " + array[-1]
     end
   end
   return new_string
