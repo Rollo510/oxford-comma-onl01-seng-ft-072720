c9efbbdad8dd0a69c399b21cbbfab8951bc9e1d8
Kevin Britt <kbritt510@gmail.com>
2020-07-29 18:52:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,10 @@
 def oxford_comma(array)
   array.each do |element|
     if array.size == 1
+      element.join("")
+    elsif array.size == 2
+      element.join("and")
+    elsif array.size == 3
+      element
   end
 end
