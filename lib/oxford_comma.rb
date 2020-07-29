259d0f391cc36e33e35b9835d4bfb0644838ebc5
Kevin Britt <kbritt510@gmail.com>
2020-07-29 19:05:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   new_string = ""
   array.each do |element|
   if array.length == 1
-    new_string.push(element)
+    new_string.push(array.join)
     end
   end
   return new_string
